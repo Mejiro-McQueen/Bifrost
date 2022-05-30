6071649ea032d581f7270ceac012d128c5f03a2f
Adrian Vazquez <adrian.vazquez@jpl.nasa.gov>
2022-05-30 04:07:00 -0700

    Add Graphable capability to most used Plugins

--- ait/core/server/plugins/PacketPadder.py
+++ ait/core/server/plugins/PacketPadder.py
@@ -1,14 +1,25 @@
 from ait.core.server.plugins import Plugin
 from ait.core import log
-
 import ait.dsn.plugins.TCTF_Manager as tctf
+import ait.dsn.plugins.Graffiti as Graffiti
 
-class PacketPadder(Plugin):
+
+class PacketPadder(Plugin,
+                   Graffiti.Graphable):
     def __init__(self, inputs=None, outputs=None, zmq_args=None, **kwargs):
         super().__init__(inputs, outputs, zmq_args)
         self.logger_header = __name__ + " ->"
         self.size_pad_octets = tctf.get_max_data_field_size()
+        Graffiti.Graphable.__init__(self)
 
+    def graffiti(self):
+        n = Graffiti.Node(self.self_name,
+                          inputs=[(i, "Command Packets") for i in self.inputs],
+                          outputs=[],
+                          label="Pad Accumulation of Command Packets",
+                          node_type=Graffiti.Node_Type.PLUGIN)
+        return [n]
+        
     def process(self, data, topic=None):
         if not data:
             log.error(f"{self.log_header} received no data from {topic}.")
@@ -20,15 +31,3 @@ class PacketPadder(Plugin):
         if not tctf.check_data_field_size(data):
             log.error(f"{self.logger} created oversized data.")
         self.publish(data)
-
-    def graffiti(self):
-        self_name = type(self).__name__
-        nodes = []
-        labels = {}
-        for i in self.inputs:
-            labels[(i,self_name)] = "Unpadded Packet"
-        labels[self_name] = (f"\t Pad Size Octets: {self.pad_octets}")
-        node = Graffiti.Node(self_name, self.inputs, [], labels,
-                             Graffiti.Node_Type.PLUGIN)
-        nodes.append(node)
-        return nodes
