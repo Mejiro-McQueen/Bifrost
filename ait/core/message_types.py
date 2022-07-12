8f70723fdc95eaeeb6f1cb3543c99acde1290e81
Adrian Vazquez <adrian.vazquez@jpl.nasa.gov>
2022-07-11 19:23:57 -0700

    Add File Manager Types

--- ait/core/message_types.py
+++ ait/core/message_types.py
@@ -1,4 +1,6 @@
 from enum import Enum
+from dataclasses import dataclass
+
 class MessageType(Enum):
     """
     Use these enums (pubsub topics) when the type of data published is more important than the module who published it. (e.g, OpenMCT doesn't care who vcid_router plugin is, but it certainly cares about the VCID_COUNT it periodically publishes.)
@@ -31,3 +33,32 @@ class MessageType(Enum):
     KMC_STATUS = "If message is received\n, KMC Plugin is active"
     SC_STATE_OF_HEALTH_REPORT = "Spacecraft state of health report"
     GRAFFITI_MAP = "DOT file containing AIT Pipeline" # TODO Do we want this, and does Joe want a DOT or PNG?
+    TASK_FILE_DOWNLINK_RESULT = "Result of a File Downlink Reassembly Task"
+    TASK_FILE_DOWNLINK_UPDATE = "Result of a File Dowlink Update Task"
+    TASK_S3_UPLOAD_RESULT = ""
+    
+class Task_Message():
+    def __init__(self, _id, _metadata):
+        self._id = _id
+        self._metadata = _metadata
+        self.result = None
+        self.status = "pending"
+        self.name = self.__class__.__name__
+
+class File_Reassembly_Task(Task_Message):
+    """ This task is run automatically with ID 0"""
+    def __init__(self, _id, path):
+        _metadata = {'path': path}
+        _id = 0
+        Task_Message.__init__(self, _id, _metadata)
+
+class S3_File_Upload_Task(Task_Message):
+    """
+    Request FileManager to upload local path to S3 bucket.
+    If this task has ID 0, then it was run automatically by AIT from a File_Reassembly_Task.
+    """
+    def __init__(self, _id, path, url):
+        _metadata = {'url': str(url),
+                     'path': path}
+        Task_Message.__init__(self, _id, _metadata)
+                
