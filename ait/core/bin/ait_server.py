b16273ba42eed0a320596b2224ff562f2215a041
Adrian Vazquez <adrian.vazquez@jpl.nasa.gov>
2022-10-17 17:41:14 -0700

    Acquire flock on startup
    Simultaneous AIT instances will cause undefined behavior (mostly zmq
    and socket).
    
    e.g. a single command can result in multiple encrypted TCTF, one DSN
    bind invocation can be sent multiple times from multiple sockets.

--- ait/core/bin/ait_server.py
+++ ait/core/bin/ait_server.py
@@ -1,5 +1,9 @@
 #!/usr/bin/env python
 
+import fcntl
+import os
+import sys
+
 """
 Usage: ait-server
 
@@ -18,9 +22,21 @@ def main():
     )
     args = ap.parse_args()  # noqa
 
+    if running():
+        print("An instance of AIT is already running or could not obtain /tmp/ait_server.lock flock.")
+        sys.exit(-1)
+    
     tlm_cmd_serv = Server()
     tlm_cmd_serv.wait()
 
-
 if __name__ == "__main__":
     main()
+
+def running():
+    lock_file = os.open(f"/tmp/ait_server.lock", os.O_WRONLY | os.O_CREAT)
+    try:
+        fcntl.lockf(lock_file, fcntl.LOCK_EX | fcntl.LOCK_NB)
+        already_running = False
+    except IOError:
+        already_running = True
+    return already_running
