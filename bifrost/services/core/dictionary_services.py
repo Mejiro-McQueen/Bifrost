9389dc1dcf073ef75f909ce56f187db52cd6010b
Adrian Vazquez <adrian.vazquez@jpl.nasa.gov>
2023-04-11 20:50:06 -0700

    custom cltu support
    fix logging message
    
    remove debug prints
    
    Remove import
    
    fixup cltu customization

--- bifrost/services/core/dictionary_services.py
+++ bifrost/services/core/dictionary_services.py
@@ -1,4 +1,5 @@
 from bifrost.common.service import Service
+from bifrost.common.loud_exception import with_loud_coroutine_exception, with_loud_exception
 from ait.core import cmd, tlm
 from colorama import Fore
 from ait.core import log
@@ -6,11 +7,13 @@ import traceback
 
 
 class Command_Dictionary_Service(Service):
+    @with_loud_exception
     def __init__(self):
         super().__init__()
         self.cmd_dict = cmd.getDefaultDict()
         self.start()
 
+    @with_loud_coroutine_exception
     async def reconfigure(self, topic, message, reply):
         if message:
             await super().reconfigure(topic, message, reply)
@@ -19,6 +22,7 @@ class Command_Dictionary_Service(Service):
         log.info("Dictionaries reloaded!")
         return
 
+    @with_loud_coroutine_exception
     async def generate_command_object(self, topic, message, reply):
         # Don't actually use this, not langauge agnostic, we'll get rid of it someday.
         try:
@@ -29,6 +33,7 @@ class Command_Dictionary_Service(Service):
             res = (False, None)
         await self.publish(reply, res)
 
+    @with_loud_coroutine_exception
     async def generate_command_raw(self, topic, message, reply):
         try:
             cmd_obj = self.cmd_dict.create(message)
@@ -38,11 +43,14 @@ class Command_Dictionary_Service(Service):
             log.error(e)
             log.error(traceback.print_exc())
             res = (False, None)
+        log.info(res[1].hex())
         await self.publish(reply, res)
 
+    @with_loud_coroutine_exception
     async def get_dictionary(self, topic, message, reply):
         # Don't actually use this, not langauge agnostic, we'll get rid of it someday.
         await self.publish(reply, self.cmd_dict)
 
+    @with_loud_coroutine_exception
     async def get_dictionary_json(self, topic, message, reply):
         await self.publish(reply, self.cmd_dict.toJSON())
